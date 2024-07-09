7b8f6159aff7ae699b52e98c4a6c8c3c839ef5b5
SHIHAB .COM.CO <frontendmaker99@gmail.com>
2024-07-09 12:03:26 +0600

    Add project setup dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      // "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
